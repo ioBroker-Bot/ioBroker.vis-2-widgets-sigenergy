7af90bb0791b764c75eef3171352e1bb09a3451f
ssbingo <s.sternitzke@online.de>
2026-03-26 11:36:29 +0100

    v1.6.0

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // ioBroker prettier configuration file
 import prettierConfig from '@iobroker/eslint-config/prettier.config.mjs';
 
@@ -7,13 +6,3 @@ export default {
     // uncomment next line if you prefer double quotes
     // singleQuote: false,
 };
-=======
-// iobroker prettier configuration file
-import prettierConfig from '@iobroker/eslint-config/prettier.config.mjs';
-
-export default {
-	...prettierConfig,
-	useTabs: true,
-	singleQuote: true,
-};
->>>>>>> ff4988fcbf802fc4fe9d38947f93a8d92cc69f9a
